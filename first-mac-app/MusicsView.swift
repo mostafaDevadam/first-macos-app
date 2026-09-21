3ae6e3c5b5c1a280d7f798455a2d7db5e9ed104c
mostafaDevadam <adam.mostafa@outlook.de>
2026-09-21 08:05:25 +0300

    update drag-drop view and create musices and videos views

---
+++ first-mac-app/MusicsView.swift
@@ -0,0 +1,18 @@
+//
+//  MusicsView.swift
+//  first-mac-app
+//
+//  Created by mostafa on 21.09.26.
+//
+
+import SwiftUI
+
+struct MusicsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MusicsView()
+}
---
+++ first-mac-app/VideosView.swift
@@ -0,0 +1,18 @@
+//
+//  VideosView.swift
+//  first-mac-app
+//
+//  Created by mostafa on 21.09.26.
+//
+
+import SwiftUI
+
+struct VideosView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideosView()
+}
